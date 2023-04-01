80e9880c890330137abc8f08b6a10f5b4d395b2c
Emerlyy <vadim200339@gmail.com>
2023-04-01 11:33:55 +0300

    refactoring

--- components/ItemCard.js
+++ components/ItemCard.js
@@ -7,19 +7,19 @@ const ItemCard = ({ title, releaseDate, posterPath, maxWidth, imgHeight, imgSize
   const theme = useTheme();
 
   return (
-    <Card variant='outlined' elevation={0} sx={{ maxWidth: `${maxWidth || 300}px`, display: 'flex', flexDirection: 'column', ...sx }}>
+    <Card variant='outlined' elevation={0} sx={{ maxWidth: maxWidth || '300px', display: 'flex', flexDirection: 'column', ...sx }}>
       <CardActionArea component={Link} href={path}>
         {
           posterPath
             ? <CardMedia
               component="img"
-              sx={{ height: `${imgHeight || 450}px` }}
+              sx={{ height: imgHeight || '450px' }}
               image={movieApi.getImageUrl(posterPath, imgSize)} />
-            : <Box sx={{ width: '100%', height: imgHeight, display: 'grid', placeItems: 'center' }}><ImageOutlinedIcon sx={{ fontSize: 60 }} /></Box>
+            : <Box sx={{ width: '100%', height: imgHeight || 450, display: 'grid', placeItems: 'center' }}><ImageOutlinedIcon sx={{ fontSize: 60 }} /></Box>
         }
       </CardActionArea>
       <CardContent sx={{ display: 'flex', flexDirection: 'column', flexGrow: 1, padding: '16px 16px 0 16px', bgcolor: theme.palette.primary.main }}>
-        <Typography variant="body2" component='h5' sx={{ flexGrow: 1 }}>{title}</Typography>
+        <Typography variant="h6" component='h5' sx={{ flexGrow: 1 }}>{title}</Typography>
         <Typography variant="caption" component='span'>{releaseDate}</Typography>
       </CardContent>
     </Card>
