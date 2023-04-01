b2bff934be599a3db0bc66e84f1697daa0216416
Emerlyy <vadim200339@gmail.com>
2023-04-01 19:17:31 +0300

    fixed bugs and added zIndex

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,16 @@
 import Navbar from "./Navbar";
 
-import { AppBar, Container, IconButton, Menu, Paper, Toolbar, Typography } from "@mui/material";
+import { AppBar, Box, Container, IconButton, Menu, Paper, Toolbar, Typography } from "@mui/material";
 import LiveTvRoundedIcon from '@mui/icons-material/LiveTvRounded';
 import ThemeSwitcher from "./ThemeSwitcher";
 import { useEffect, useState } from "react";
 import MenuIcon from '@mui/icons-material/Menu';
 import CloseIcon from '@mui/icons-material/Close';
+import Link from "next/link";
 
 const Header = () => {
 
-  const [isMenuOpened, setIsMenuOpened] = useState(null);
+  const [isMenuOpened, setIsMenuOpened] = useState(false);
 
   const toggleMenu = () => {
     setIsMenuOpened(prev => !prev);
@@ -23,26 +24,13 @@ const Header = () => {
     <AppBar position="static" sx={{ borderWidth: '0px 0px thin' }} elevation={0} variant="outlined">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <IconButton
-            sx={{ mr: 0, ml: 'auto', display: { xs: 'flex', md: 'none' } }}
-            size="large"
-            onClick={toggleMenu}
-            color="inherit">
-            <MenuIcon />
-          </IconButton>
-          <Paper elevation={0} square sx={{ zIndex: 10, bgcolor: { md: 'inherit' }, width: '100%', alignItems: 'center', justifyContent: 'center', display: 'flex', flexDirection: { xs: 'column', md: 'row' }, position: { xs: 'fixed', md: 'static' }, overflow: 'hidden', top: 0, left: 0, right: 0, bottom: 0, transition: '.3s', transform: { xs: isMenuOpened ? '' : 'translateX(-100%)', md: 'none' } }}>
-            <LiveTvRoundedIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
-            <IconButton size="large" color='inherit' sx={{ display: { md: 'none' }, position: 'absolute', top: 10, right: 10 }} onClick={toggleMenu}>
-              <CloseIcon color="inherit" />
-            </IconButton>
+          <Box component={Link} href="/" sx={{ display: 'flex', alignItems: 'center', mr: 18 }}>
+            <LiveTvRoundedIcon sx={{ mr: 1 }} />
             <Typography
-              variant="h6"
+              fontSize='1rem'
+              mt='4px'
               noWrap
-              component="a"
-              href="/"
               sx={{
-                mr: 18,
-                display: { xs: 'none', md: 'flex' },
                 fontFamily: 'monospace',
                 fontWeight: 700,
                 letterSpacing: '.2rem',
@@ -52,6 +40,18 @@ const Header = () => {
             >
               MOVIES
             </Typography>
+          </Box>
+          <IconButton
+            sx={{ mr: 0, ml: 'auto', display: { xs: 'flex', md: 'none' } }}
+            size="large"
+            onClick={toggleMenu}
+            color="inherit">
+            <MenuIcon />
+          </IconButton>
+          <Paper elevation={0} square sx={{ zIndex: 1200, bgcolor: { md: 'inherit' }, width: '100%', alignItems: 'center', justifyContent: 'center', display: 'flex', flexDirection: { xs: 'column', md: 'row' }, position: { xs: 'fixed', md: 'static' }, overflow: 'hidden', top: 0, left: 0, right: 0, bottom: 0, transition: '.3s', transform: { xs: isMenuOpened ? '' : 'translateX(-100%)', md: 'none' } }}>
+            <IconButton size="large" color='inherit' sx={{ display: { md: 'none' }, position: 'absolute', top: 10, right: 10 }} onClick={toggleMenu}>
+              <CloseIcon color="inherit" />
+            </IconButton>
             <Navbar onClick={toggleMenu} sx={{ flexGrow: { xs: 0, md: 1 }, display: 'flex', alignItems: 'center', flexDirection: { xs: 'column', md: 'row' }, columnGap: 3 }} />
             <ThemeSwitcher sx={{ position: { xs: 'absolute', md: 'relative' }, bottom: { xs: 10, md: 'unset' }, right: { xs: 10, md: 'unset' } }} />
           </Paper>
