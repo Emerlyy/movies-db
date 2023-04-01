9d6e443ee8399c25e6e81b762650174c390fef0b
Emerlyy <vadim200339@gmail.com>
2023-04-01 19:24:13 +0300

    fixed bugs

--- components/Header.js
+++ components/Header.js
@@ -12,8 +12,8 @@ const Header = () => {
 
   const [isMenuOpened, setIsMenuOpened] = useState(false);
 
-  const toggleMenu = () => {
-    setIsMenuOpened(prev => !prev);
+  const toggleMenu = (newState) => {
+    setIsMenuOpened(newState);
   };
 
   useEffect(() => {
@@ -44,15 +44,15 @@ const Header = () => {
           <IconButton
             sx={{ mr: 0, ml: 'auto', display: { xs: 'flex', md: 'none' } }}
             size="large"
-            onClick={toggleMenu}
+            onClick={() => toggleMenu(true)}
             color="inherit">
             <MenuIcon />
           </IconButton>
           <Paper elevation={0} square sx={{ zIndex: 1200, bgcolor: { md: 'inherit' }, width: '100%', alignItems: 'center', justifyContent: 'center', display: 'flex', flexDirection: { xs: 'column', md: 'row' }, position: { xs: 'fixed', md: 'static' }, overflow: 'hidden', top: 0, left: 0, right: 0, bottom: 0, transition: '.3s', transform: { xs: isMenuOpened ? '' : 'translateX(-100%)', md: 'none' } }}>
-            <IconButton size="large" color='inherit' sx={{ display: { md: 'none' }, position: 'absolute', top: 10, right: 10 }} onClick={toggleMenu}>
+            <IconButton size="large" color='inherit' sx={{ display: { md: 'none' }, position: 'absolute', top: 10, right: 10 }} onClick={() => toggleMenu(false)}>
               <CloseIcon color="inherit" />
             </IconButton>
-            <Navbar onClick={toggleMenu} sx={{ flexGrow: { xs: 0, md: 1 }, display: 'flex', alignItems: 'center', flexDirection: { xs: 'column', md: 'row' }, columnGap: 3 }} />
+            <Navbar onClick={() => toggleMenu(false)} sx={{ flexGrow: { xs: 0, md: 1 }, display: 'flex', alignItems: 'center', flexDirection: { xs: 'column', md: 'row' }, columnGap: 3 }} />
             <ThemeSwitcher sx={{ position: { xs: 'absolute', md: 'relative' }, bottom: { xs: 10, md: 'unset' }, right: { xs: 10, md: 'unset' } }} />
           </Paper>
         </Toolbar>
