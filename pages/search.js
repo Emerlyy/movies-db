65c0ec289f1f9287acaf04364e371be06b212588
Emerlyy <vadim200339@gmail.com>
2023-04-03 11:03:08 +0300

    adapted for small screens

--- pages/search.js
+++ pages/search.js
@@ -82,8 +82,8 @@ const Search = ({ query: initialQuery, results, total_pages, page, type }) => {
         onLoaderFinished={clearProgress} />
       <Paper sx={{ py: 6, flexGrow: 1 }}>
         <Container maxWidth="lg">
-          <Box mb={4} sx={{ display: 'flex', justifyContent: 'space-between' }}>
-            <TextField sx={{ width: '70%' }} size='small' color='text' autoComplete="off" value={query} onChange={(e) => handleQueryChange(e.target.value)} label='Search'></TextField>
+          <Box mb={4} sx={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap-reverse', gap: 2 }}>
+            <TextField sx={{ width: '70%', flexGrow: 1 }} size='small' color='text' autoComplete="off" value={query} onChange={(e) => handleQueryChange(e.target.value)} label='Search'></TextField>
             <ToggleButtonGroup
               value={type}
               size='small'
@@ -102,7 +102,7 @@ const Search = ({ query: initialQuery, results, total_pages, page, type }) => {
                     results.map(itm => <SearchCard key={itm.id} item={itm} type={type === 'movies' ? 'movie' : 'tv'} />)
                   }
                 </Box>
-                <Pagination sx={{ marginX: 'auto', width: 'fit-content' }} siblingCount={3} count={Math.min(total_pages, 500)} variant="outlined" shape="rounded" page={page} onChange={handlePageChange} />
+                <Pagination sx={{ mt: 4, marginX: 'auto', width: 'fit-content' }} size='small' siblingCount={1} count={Math.min(total_pages, 500)} variant="outlined" shape="rounded" page={page} onChange={handlePageChange} />
               </>
               : <>{!isWaiting && !progress && <Typography>{query ? 'There are no movies that matched your query' : 'Start typing to see results'}</Typography>}</>
           }
