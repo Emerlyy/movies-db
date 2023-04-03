4f75eb41678fcea15a701a11d9ff61920847e953
Emerlyy <vadim200339@gmail.com>
2023-04-03 10:59:58 +0300

    adapted for small screens

--- components/ItemInfo.js
+++ components/ItemInfo.js
@@ -19,15 +19,15 @@ const ItemInfo = ({ item }) => {
   const duration = calculateRuntime(runtime);
 
   return (
-    <Paper elevation={0} sx={{ mx: 'auto', my: 6, width: 1000, overflow: 'hidden', bgcolor: 'transparent', color: '#fff' }}>
-      <Box sx={{ p: 4, width: '100%', display: 'flex', columnGap: 3, bgcolor: 'transparent', backdropFilter: 'blur(5px)', backgroundImage: `linear-gradient(to right, rgba(20, 20, 20, 1), rgba(20, 20, 20, 0.7) 65%, rgba(20, 20, 20, 0.7) 100%)` }}>
+    <Paper elevation={0} sx={{ mx: 'auto', my: 6, overflow: 'hidden', bgcolor: 'transparent', color: '#fff' }}>
+      <Box sx={{ p: { xs: 2, md: 4 }, width: '100%', display: 'flex', flexDirection: { xs: 'column', md: 'row' }, alignItems: { xs: 'center', md: 'flex-start' }, columnGap: 3, bgcolor: 'transparent', backdropFilter: 'blur(5px)', backgroundImage: `linear-gradient(to right, rgba(20, 20, 20, 1), rgba(20, 20, 20, 0.7) 65%, rgba(20, 20, 20, 0.7) 100%)` }}>
         <Image alt='Poster' src={poster} width={300} height={450} style={{ borderRadius: '2px' }} />
-        <Box>
-          <Box mb={3} sx={{ display: 'flex', flexWrap: 'wrap', alignItems: 'center', columnGap: 1 }}>
-            <Typography sx={{ width: '100%', fontWeight: 700 }} component='h2' variant='h4'>
+        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: { xs: 'center', md: 'flex-start' } }}>
+          <Box mb={3} sx={{ display: 'flex', flexDirection: { xs: 'column', sm: 'row' }, flexWrap: 'wrap', justifyContent: { xs: 'center', md: 'flex-start' }, alignItems: 'center', columnGap: 1 }}>
+            <Typography mt={{ xs: 1, md: 0 }} sx={{ width: '100%', fontWeight: 700, textAlign: { xs: 'center', md: 'start' } }} component='h2' variant='h4'>
               {title} <Typography component='span' variant='h4' color='gray'>({year})</Typography>
             </Typography>
-            <Typography component='span' variant="body2">{day}/{month}/{year}</Typography>
+            <Typography mt={{ xs: 2, sm: 0 }} component='span' variant="body2">{day}/{month}/{year}</Typography>
             <CircleIcon sx={{ fontSize: 6 }} />
             <Box sx={{ display: 'flex', columnGap: 1 }}>
               {
@@ -37,14 +37,14 @@ const ItemInfo = ({ item }) => {
             <CircleIcon sx={{ fontSize: 6 }} />
             {duration && <Typography component='span' variant="body2">{duration.hours}h {duration.minutes}m</Typography>}
           </Box>
-          <Box mb={5} sx={{ display: 'flex', alignItems: 'center' }}>
+          <Box mb={{ xs: 2, sm: 5 }} sx={{ display: 'flex', alignItems: 'center' }}>
             <Box
               onMouseEnter={handlePopoverOpen}
               onMouseLeave={handlePopoverClose}
               mr={2}
-              sx={{ width: 70, height: 70, bgcolor: '#081c22', borderRadius: '50%', p: .5 }}>
+              sx={{ width: { xs: 40, sm: 70 }, height: { xs: 40, sm: 70 }, bgcolor: '#081c22', borderRadius: '50%', p: .5 }}>
               <CircularProgressbarWithChildren styles={buildStyles({ pathColor: "#21d07a", trailColor: '#204529', backgroundColor: '#3e98c7' })} value={vote_average} maxValue={10}>
-                <Typography component='span' variant="h5" fontWeight='bold'>{Math.round(vote_average * 10)}</Typography>
+                <Typography component='span' variant="progress" fontWeight='bold'>{Math.round(vote_average * 10)}</Typography>
               </CircularProgressbarWithChildren>
             </Box>
             <Typography fontWeight='bold' component='span' variant="body2">User<br />Score</Typography>
@@ -70,7 +70,7 @@ const ItemInfo = ({ item }) => {
               <Typography px={2} py={1}>{vote_count} votes</Typography>
             </Popover>
           </Box>
-          <Typography component='span' variant="subtitle1" sx={{ fontStyle: 'italic' }} >{tagline}</Typography>
+          <Typography component='span' variant="tagline" sx={{ fontStyle: 'italic' }} >{tagline}</Typography>
           <Box mt={5}>
             <Typography mb={1} variant="h6" component='h3'>Overview</Typography>
             <Typography variant="body2" component='p'>{overview}</Typography>
